b2c31e358f1636545741b4ad4a64bbe70d3b803b
Tattoomii <hello@tattoomii.com>
2026-02-06 02:25:11 +0100

    Fix: Remove static export, rebuild for proper CSS

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  /* config options here */
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 export default nextConfig;
